14cd46727b49bb1a05319f3da4375b44314699a7
Gohar Irfan <goharirfan94@gmail.com>
2015-03-23 16:05:01 +0500

    deployed on heroku.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,26 +8,57 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     @all_ratings = Movie.all_ratings
     @checked_ratings = @all_ratings
-    
+    doRedirect = false
     # for sorting on title and date
-    if (params.has_key?(:sort_by))
-      if params[:sort_by] == "title"
-        @movies.sort_by! {|obj| obj.title}
-      elsif params[:sort_by] == "release_date"
-        @movies.sort_by! {|obj| obj.release_date}
+    # case3 = if nothing set, doesnt do anything
+    if (params.has_key?(:sort_by) || session.has_key?(:sort_by))
+      # case1 = session set, param not set
+      if (session.has_key?(:sort_by) && !params.has_key?(:sort_by))
+        params[:sort_by] = session[:sort_by]
+        doRedirect = true
+      # case 2 = both set, set session to params
+      elsif (session.has_key?(:sort_by) && params.has_key?(:sort_by))
+        session[:sort_by] = params[:sort_by]
+      end
+      
+      if not doRedirect
+        if params[:sort_by] == "title"
+          @movies.sort_by! {|obj| obj.title}
+        elsif params[:sort_by] == "release_date"
+          @movies.sort_by! {|obj| obj.release_date}
+        end
       end
     end
+    session[:sort_by] = params[:sort_by]
 
     # for filtering on ratings
-    if (params.has_key?(:ratings))
-      ratings_temp = params[:ratings]
-      ratings = []
-      for x in ratings_temp do
-        ratings.append(x[0].to_s)
+    # case3 = if nothing set, doesnt do anything
+    if (params.has_key?(:ratings) || session.has_key?(:ratings))
+      # case1 = session set, param not set
+      if (session.has_key?(:ratings) && !params.has_key?(:ratings))
+        params[:ratings] = session[:ratings]
+        doRedirect = true
+      # case 2 = both set, set session to params
+      elsif (session.has_key?(:ratings) && params.has_key?(:ratings))
+        session[:ratings] = params[:ratings]
       end
-      @checked_ratings = ratings
-      ratings_filtered_movies = Movie.find_all_by_rating(ratings)
-      @movies = @movies & ratings_filtered_movies
+
+      if not doRedirect
+        ratings_temp = params[:ratings]
+        ratings = []
+        for x in ratings_temp do
+          ratings.append(x[0].to_s)
+        end
+        @checked_ratings = ratings
+        ratings_filtered_movies = Movie.find_all_by_rating(ratings)
+        @movies = @movies & ratings_filtered_movies
+      end
+    end
+    session[:ratings] = params[:ratings]
+
+    if doRedirect
+      flash.keep
+      redirect_to :action => 'index', :ratings => session[:ratings], :sort_by => params[:sort_by]
     end
   
   end
